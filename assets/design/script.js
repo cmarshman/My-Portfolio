ca5deb60365803faf72b22dfd081cf9a01fd283b
Cory Marshman <corymarshman@Corys-MacBook-Air.local>
2019-12-15 00:18:50 -0500

    HTML and Java updates

--- assets/design/script.js
+++ assets/design/script.js
@@ -1,39 +1,62 @@
-const modal = $("#newsDisplay");
 const projectArray = {
-    "portfolio1": {
-    name : "Intuitive Banana",
-    "https://drbailey13.github.io/IntuitiveBanana/index.html",
-    "https://github.com/DrBailey13/IntuitiveBanana/tree/master",
-    "../images/intuitive_banana/home_page/home_page1.png",
-    "../images/intuitive_banana/home_page/home_page2.png",
-    "../images/intuitive_banana/featured_artist/featured_artist1.png",
-    "../images/intuitive_banana/featured_artist/featured_artist2.png"
+    portfolio1: {
+        name: "Intuitive Banana",
+        deployedURL: "https://drbailey13.github.io/IntuitiveBanana/index.html",
+        githubURL: "https://github.com/cmarshman/IntuitiveBanana",
+        images: {
+            homePage: {
+                one: "/assets/images/intuitive_banana/home_page/home_page1.png",
+                two: "assets/images/intuitive_banana/home_page/home_page2.png"
+            },
+            featuredArtists: {
+                one: "assets/images/intuitive_banana/featured_artist/featured_artist1.png",
+                two: "assets/images/intuitive_banana/featured_artist/featured_artist2.png"
+            },
+            musicNews: {
+                one: "assets/images/intuitive_banana/music_news/music_news1.png",
+                two: "assets/images/intuitive_banana/music_news/music_news2.png",
+                three: "assets/images/intuitive_banana/music_news/music_news3.png",
+            },
+            searchPage: {
+                one: "assets/images/intuitive_banana/search/search1.png",
+                two: "assets/images/intuitive_banana/search/search2.png",
+                three: "assets/images/intuitive_banana/search/search3.png",
+            },
+        },
     },
-    "portfolio2": [
-        "day_planner",
-        "https://cmarshman.github.io/day_planner/index.html",
-        "https://github.com/cmarshman/calendar",
-        "../images/day_planner/page_opening.png",
-        "../images/day_planner/local_storage.png",
-        "../images/day_planner/moments_color_change.png",
-        "../images/day_planner/return_to_page.png"
-    ],
-    "portfolio3": [
-        "password_generator",
-        "https://cmarshman.github.io/passwordgenerator/index.html",
-        "https://github.com/cmarshman/passwordgenerator",
-        "../images/password_generator/beginning_prompt.png",
-        "../images/password_generator/password_generated.png"
-    ],
+    portfolio2: {
+        name: "day_planner",
+        deployedURL: "https://cmarshman.github.io/day_planner/index.html",
+        githubURL: "https://github.com/cmarshman/calendar",
+        images: {
+            one: "assets/images/day_planner/page_opening.png",
+            two: "assets/images/day_planner/local_storage.png",
+            three: "assets/images/day_planner/moments_color_change.png",
+            four: "assets/images/day_planner/return_to_page.png"
+        },
+    },
+    portfolio3: {
+        name: "password_generator",
+        deployedURL: "https://cmarshman.github.io/passwordgenerator/index.html",
+        githubURL: "https://github.com/cmarshman/passwordgenerator",
+        images: {
+            one: "assets/images/password_generator/beginning_prompt.png",
+            two: "assets/images/password_generator/password_generated.png",
+        },
+    },
 };
+const modal = $("#projectDisplay");
 
-
-$(".project").on("click",function() {
+$(".project").on("click", function () {
     const portfolio = $(this).attr("data-portfolio");
     console.log(portfolio);
-    $("#displayTitle").text(projectArray.portfolio1[0])
+    $("#displayName").text(projectArray.portfolio1.name);
+    $("img1").attr("src", projectArray.portfolio1.images.homePage.one);
+    $("#displayURL").attr("href", projectArray.portfolio1.deployedURL).text("Deployed URL");
+    $("#displayURL2").attr("href", projectArray.portfolio1.githubURL).text("GitHub Repo");
+    modal.addClass('is-active');
 });
 
-$(".close-modal").on("click",function(){
+$(".close-modal").on("click", function () {
     modal.removeClass("is-active");
 });
